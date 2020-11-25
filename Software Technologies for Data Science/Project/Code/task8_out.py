f0e3ac35d5645ce7eb8bb6b52a78f89876b2dbdc
Neel Patel <neelp@hotmail.co.uk>
2020-11-25 22:07:29 +0000

    Adding the offline exporting CSV functionality

--- Software Technologies for Data Science/Project/Code/task8_out.py
+++ Software Technologies for Data Science/Project/Code/task8_out.py
@@ -3,4 +3,20 @@ import csv
 import datetime
 import sqlite3
 
-db = sqlite3.connect('')
+db = sqlite3.connect('database.db')
+cursor = db.cursor()
+
+vehicle_list = ['car', 'taxi', 'bus', 'bicycle', 'motorbike', 'van', 'truck', 'other']
+writing_to_csv = []
+valid_input = False
+
+while valid_input == False:
+
+    try:
+        start_date = datetime.datetime.strptime(input('Type a start date YEAR-MONTH-DAY-HOUR-MINUTE in the format YYYYMMDDHHMM: '), '%Y%m%d%H%M')
+        end_date = datetime.datetime.strptime(input('Type a end date YEAR-MONTH-DAY-HOUR-MINUTE in the format YYYYMMDDHHMM: '), '%Y%m%d%H%M')
+        valid_input == True
+    
+    except:
+        print('Incorrect time format given, please provide a valid date format.')
+
