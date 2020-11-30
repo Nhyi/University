0392f26cf5532664c25ec73af42d5a67f2293170
Neel Patel <neelp@hotmail.co.uk>
2020-11-30 15:25:28 +0000

    Finishing off hour calculation for task 9

--- Software Technologies for Data Science/Project/Code/task9_out.py
+++ Software Technologies for Data Science/Project/Code/task9_out.py
@@ -1,6 +1,7 @@
 #importing the required modules
 import csv
 import sqlite3
+from math import ceil
 from datetime import datetime, timedelta
 from dateutil.relativedelta import relativedelta
 
@@ -26,28 +27,28 @@ day = day.strftime('%Y-%m-%d')
 week = week.strftime('%Y-%m-%d')
 month = month.strftime('%Y-%m-%d')
 
-cursor.execute('''SELECT username, ROUND(SUM((julianday(end_time) - julianday(start_time)) * 24), 1) FROM loginsession WHERE end_time IS NOT NULL AND date(start_time) = ? GROUP BY username''', (day,))
+cursor.execute('''SELECT username, (SUM(julianday(end_time) - julianday(start_time))) FROM loginsession WHERE end_time IS NOT NULL AND date(start_time) = ? GROUP BY username''', (day,))
 day_records = cursor.fetchall()
 name_list = []
 day_list = []
 
-cursor.execute('''SELECT username, ROUND(SUM((julianday(end_time) - julianday(start_time)) * 24), 1) FROM loginsession WHERE end_time IS NOT NULL AND date(start_time) BETWEEN ? AND ? GROUP BY username''', (week, day))
+cursor.execute('''SELECT username, (SUM(julianday(end_time) - julianday(start_time))) FROM loginsession WHERE end_time IS NOT NULL AND date(start_time) BETWEEN ? AND ? GROUP BY username''', (week, day))
 week_records = cursor.fetchall()
 week_list = []
 
-cursor.execute('''SELECT username, ROUND(SUM((julianday(end_time) - julianday(start_time)) * 24), 1) FROM loginsession WHERE end_time IS NOT NULL AND date(start_time) BETWEEN ? AND ? GROUP BY username''', (month, day))
+cursor.execute('''SELECT username, (SUM(julianday(end_time) - julianday(start_time))) FROM loginsession WHERE end_time IS NOT NULL AND date(start_time) BETWEEN ? AND ? GROUP BY username''', (month, day))
 month_records = cursor.fetchall()
 month_list = []
 
 for record in day_records:
     name_list.append(record[0])
-    day_list.append(record[1])
+    day_list.append(ceil((record[1] * 24) * 10) / 10)
 
 for record in week_records:
-    week_list.append(record[1])
+    week_list.append(ceil((record[1] * 24) * 10) / 10)
 
 for record in month_records:
-    month_list.append(record[1])
+    month_list.append(ceil((record[1] * 24) * 10) / 10)
 
 complete_list = list(zip(name_list, day_list, week_list, month_list))
 print(complete_list)
